1c6575462d9028252434e5e9b6d2272a5ea333b1
fwldom <fwldom@duck.com>
2024-05-02 10:55:29 +0330

    Upload Files

---
+++ Freedom-Guard/FreedomGuard.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Freedom_Guard
+{
+    partial class FreedomGuard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FreedomGuard));
+            this.start_Guard = new System.Windows.Forms.Button();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.languagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.persianفارسیToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Stop_Guard = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.StatusStrip();
+            this.ProgressBarStatus = new System.Windows.Forms.ToolStripProgressBar();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.exitAndStopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LogApp = new System.Windows.Forms.TextBox();
+            this.LabelLogApp = new System.Windows.Forms.Label();
+            this.Gool_services = new System.Windows.Forms.Button();
+            this.MainMenu.SuspendLayout();
+            this.Status.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start_Guard
+            // 
+            this.start_Guard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.start_Guard.Location = new System.Drawing.Point(12, 35);
+            this.start_Guard.Name = "start_Guard";
+            this.start_Guard.Size = new System.Drawing.Size(85, 38);
+            this.start_Guard.TabIndex = 0;
+            this.start_Guard.Text = "Start";
+            this.start_Guard.UseVisualStyleBackColor = true;
+            this.start_Guard.Click += new System.EventHandler(this.start_Guard_Click);
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programToolStripMenuItem,
+            this.languagesToolStripMenuItem});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(764, 24);
+            this.MainMenu.TabIndex = 1;
+            this.MainMenu.TabStop = true;
+            this.MainMenu.Text = "MainMenu";
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitAndStopToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.programToolStripMenuItem.Text = "Program";
+            // 
+            // languagesToolStripMenuItem
+            // 
+            this.languagesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.englishToolStripMenuItem,
+            this.persianفارسیToolStripMenuItem});
+            this.languagesToolStripMenuItem.Name = "languagesToolStripMenuItem";
+            this.languagesToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.languagesToolStripMenuItem.Text = "Languages";
+            // 
+            // englishToolStripMenuItem
+            // 
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.englishToolStripMenuItem.Text = "English";
+            this.englishToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
+            // 
+            // persianفارسیToolStripMenuItem
+            // 
+            this.persianفارسیToolStripMenuItem.Name = "persianفارسیToolStripMenuItem";
+            this.persianفارسیToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.persianفارسیToolStripMenuItem.Text = "Persian (فارسی)";
+            this.persianفارسیToolStripMenuItem.Click += new System.EventHandler(this.persianفارسیToolStripMenuItem_Click);
+            // 
+            // Stop_Guard
+            // 
+            this.Stop_Guard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Stop_Guard.Location = new System.Drawing.Point(103, 35);
+            this.Stop_Guard.Name = "Stop_Guard";
+            this.Stop_Guard.Size = new System.Drawing.Size(85, 38);
+            this.Stop_Guard.TabIndex = 2;
+            this.Stop_Guard.Text = "Stop";
+            this.Stop_Guard.UseVisualStyleBackColor = true;
+            this.Stop_Guard.Click += new System.EventHandler(this.Stop_Guard_Click);
+            // 
+            // Status
+            // 
+            this.Status.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ProgressBarStatus,
+            this.StatusLabel});
+            this.Status.Location = new System.Drawing.Point(0, 383);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(764, 22);
+            this.Status.TabIndex = 3;
+            this.Status.Text = "statusStrip1";
+            // 
+            // ProgressBarStatus
+            // 
+            this.ProgressBarStatus.Name = "ProgressBarStatus";
+            this.ProgressBarStatus.Size = new System.Drawing.Size(100, 16);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Overflow = System.Windows.Forms.ToolStripItemOverflow.Always;
+            this.StatusLabel.Size = new System.Drawing.Size(87, 17);
+            this.StatusLabel.Text = "Not Running ...";
+            // 
+            // exitAndStopToolStripMenuItem
+            // 
+            this.exitAndStopToolStripMenuItem.Name = "exitAndStopToolStripMenuItem";
+            this.exitAndStopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitAndStopToolStripMenuItem.Text = "Stop And Exit";
+            this.exitAndStopToolStripMenuItem.Click += new System.EventHandler(this.exitAndStopToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // LogApp
+            // 
+            this.LogApp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogApp.Location = new System.Drawing.Point(408, 208);
+            this.LogApp.Multiline = true;
+            this.LogApp.Name = "LogApp";
+            this.LogApp.Size = new System.Drawing.Size(344, 172);
+            this.LogApp.TabIndex = 4;
+            // 
+            // LabelLogApp
+            // 
+            this.LabelLogApp.AutoSize = true;
+            this.LabelLogApp.Location = new System.Drawing.Point(408, 189);
+            this.LabelLogApp.Name = "LabelLogApp";
+            this.LabelLogApp.Size = new System.Drawing.Size(80, 13);
+            this.LabelLogApp.TabIndex = 5;
+            this.LabelLogApp.Text = "Log Application";
+            // 
+            // Gool_services
+            // 
+            this.Gool_services.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Gool_services.Location = new System.Drawing.Point(621, 27);
+            this.Gool_services.Name = "Gool_services";
+            this.Gool_services.Size = new System.Drawing.Size(131, 38);
+            this.Gool_services.TabIndex = 6;
+            this.Gool_services.Text = "Gool";
+            this.Gool_services.UseVisualStyleBackColor = true;
+            this.Gool_services.Click += new System.EventHandler(this.Gool_services_Click);
+            // 
+            // FreedomGuard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 405);
+            this.Controls.Add(this.Gool_services);
+            this.Controls.Add(this.LabelLogApp);
+            this.Controls.Add(this.LogApp);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Stop_Guard);
+            this.Controls.Add(this.start_Guard);
+            this.Controls.Add(this.MainMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MainMenu;
+            this.Name = "FreedomGuard";
+            this.Text = "Freedom Guard";
+            this.Load += new System.EventHandler(this.FreedomGuard_Load);
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.Status.ResumeLayout(false);
+            this.Status.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_Guard;
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem languagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem persianفارسیToolStripMenuItem;
+        private System.Windows.Forms.Button Stop_Guard;
+        private System.Windows.Forms.StatusStrip Status;
+        private System.Windows.Forms.ToolStripProgressBar ProgressBarStatus;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.ToolStripMenuItem exitAndStopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.TextBox LogApp;
+        private System.Windows.Forms.Label LabelLogApp;
+        private System.Windows.Forms.Button Gool_services;
+    }
+}
+
---
+++ Freedom-Guard/FreedomGuard.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+namespace Freedom_Guard
+{
+    public partial class FreedomGuard : Form
+    {
+        public FreedomGuard()
+        {
+            InitializeComponent();
+        }
+        Process process = null;
+        private void Log(string textlog)
+        {
+            LogApp.AppendText(textlog + Environment.NewLine);
+        }
+        public string Switchs = "";
+        private void start_Guard_Click(object sender, EventArgs e)
+        {
+            // Start Application
+            string exePath = "warp-plus.exe";
+            Stop_Guard.PerformClick();
+            if (System.IO.File.Exists(exePath))
+            {
+                ProgressBarStatus.Value = 10;
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = exePath,
+                    UseShellExecute = true,
+                    Arguments = Switchs
+                };
+                ProgressBarStatus.Value = 30;
+                process = Process.Start(startInfo);
+                Log("Start Services ...");
+                StatusLabel.Text = "Running Warp + " + services + " ...";
+                string proxyAddress = "127.0.0.1";
+                string proxyPort = "8086";
+                bool proxyEnabled = true;
+                ProgressBarStatus.Value = 100;
+                SetProxySettings(proxyAddress, proxyPort, proxyEnabled);
+                ProgressBarStatus.Value = 0;
+                Log("Set Proxy Server ...");
+            }
+            else
+            {
+                MessageBox.Show("Not Started");
+                ProgressBarStatus.Value = 0;
+                Log("Error Not Start Services ...");
+            }
+
+        }
+        static void SetProxySettings(string proxyAddress, string proxyPort, bool proxyEnabled)
+        {
+            string registryKey = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings";
+            int proxyEnabledValue = proxyEnabled ? 1 : 0;
+            string proxyServerValue = $"{proxyAddress}:{proxyPort}";
+            Registry.SetValue(registryKey, "ProxyServer", proxyServerValue);
+            Registry.SetValue(registryKey, "ProxyEnable", proxyEnabledValue);
+            Registry.SetValue(registryKey, "ProxyServerFlags", 3);
+        }
+        private void Stop_Guard_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                process.Kill();
+                ProgressBarStatus.Value = 100;
+                string proxyAddress = "127.0.0.1";
+                string proxyPort = "8086";
+                bool proxyEnabled = false;
+                Log("Stoped All Services ...");
+                SetProxySettings(proxyAddress, proxyPort, proxyEnabled);
+                ProgressBarStatus.Value = 0;
+                Log("Not Set Proxy Server ...");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void exitAndStopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stop_Guard.PerformClick();
+            Application.Exit();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stop_Guard.PerformClick();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        public string Lang = "en";
+        private void FreedomGuard_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var INIApp = new IniFile("Settings.ini");
+                Lang = INIApp.Read("lang", "setting");
+            }
+            catch
+            {
+                Lang = "en";
+
+            }
+            if (Lang == "fa")
+            {
+                start_Guard.Text = "شروع";
+                Stop_Guard.Text = "متوقف";
+                LabelLogApp.Text = "رویداد های برنامه";
+                programToolStripMenuItem.Text = "برنامه";
+                languagesToolStripMenuItem.Text = "زبان";
+                exitAndStopToolStripMenuItem.Text = "خروج و متوقف کردن سرویس";
+                exitToolStripMenuItem.Text = "خروج";
+                stopToolStripMenuItem.Text = "متوقف کردن سرویس";
+                Gool_services.Text = "سرویس گول";
+                MainMenu.RightToLeft = RightToLeft.Yes;
+            }
+        }
+        class IniFile   // revision 11
+        {
+            string Path;
+            string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+            public IniFile(string IniPath = null)
+            {
+                Path = new FileInfo(IniPath ?? EXE + ".ini").FullName;
+            }
+
+            public string Read(string Key, string Section = null)
+            {
+                var RetVal = new StringBuilder(255);
+                GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+                return RetVal.ToString();
+            }
+
+            public void Write(string Key, string Value, string Section = null)
+            {
+                WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+            }
+
+            public void DeleteKey(string Key, string Section = null)
+            {
+                Write(Key, null, Section ?? EXE);
+            }
+
+            public void DeleteSection(string Section = null)
+            {
+                Write(null, null, Section ?? EXE);
+            }
+
+            public bool KeyExists(string Key, string Section = null)
+            {
+                return Read(Key, Section).Length > 0;
+            }
+        }
+
+        private void persianفارسیToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var INIApp = new IniFile("Settings.ini");
+            INIApp.Write("lang", "fa", "setting");
+            Application.Restart();
+        }
+
+        private void englishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var INIApp = new IniFile("Settings.ini");
+            INIApp.Write("lang", "en", "setting");
+            Application.Restart();
+        }
+        public string services = "";
+        private void Gool_services_Click(object sender, EventArgs e)
+        {
+            services = "Gool";
+            Switchs = "--gool";
+            Gool_services.Text += " ✅";
+            var INIApp = new IniFile("Settings.ini");
+            INIApp.Write("gool", "true", "setting");
+        }
+    }
+}
