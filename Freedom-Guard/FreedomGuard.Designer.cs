adf965755890f715b2cc4758b2a603dea7b97015
fwldom <fwldom@duck.com>
2024-05-04 16:11:52 +0330

    Update 1.2 Full

--- Freedom-Guard/FreedomGuard.Designer.cs
+++ Freedom-Guard/FreedomGuard.Designer.cs
@@ -37,22 +37,33 @@
             this.LabelLogApp = new System.Windows.Forms.Label();
             this.Gool_services = new System.Windows.Forms.CheckBox();
             this.MenuPanel = new System.Windows.Forms.Panel();
+            this.SelectLanguage = new System.Windows.Forms.MenuStrip();
+            this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.persianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.topPanel = new System.Windows.Forms.Panel();
+            this.CloseApp = new System.Windows.Forms.Button();
             this.TextLabelTopPanel = new System.Windows.Forms.Label();
             this.MenuShowHide = new System.Windows.Forms.Button();
-            this.CloseApp = new System.Windows.Forms.Button();
+            this.StatusText = new System.Windows.Forms.Label();
+            this.scan_Warp = new System.Windows.Forms.CheckBox();
+            this.PsCountry = new System.Windows.Forms.ComboBox();
+            this.TextShowps = new System.Windows.Forms.Label();
+            this.Restart = new System.Windows.Forms.Button();
             this.Status.SuspendLayout();
+            this.MenuPanel.SuspendLayout();
+            this.SelectLanguage.SuspendLayout();
             this.topPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // start_Guard
             // 
             this.start_Guard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.start_Guard.Location = new System.Drawing.Point(249, 83);
+            this.start_Guard.Location = new System.Drawing.Point(228, 83);
             this.start_Guard.Name = "start_Guard";
-            this.start_Guard.Size = new System.Drawing.Size(85, 38);
+            this.start_Guard.Size = new System.Drawing.Size(124, 38);
             this.start_Guard.TabIndex = 0;
-            this.start_Guard.Text = "Start";
+            this.start_Guard.Text = "Connect";
             this.start_Guard.UseVisualStyleBackColor = true;
             this.start_Guard.Click += new System.EventHandler(this.start_Guard_Click);
             // 
@@ -74,10 +85,12 @@
             // 
             // StatusLabel
             // 
+            this.StatusLabel.AutoSize = false;
             this.StatusLabel.Name = "StatusLabel";
             this.StatusLabel.Overflow = System.Windows.Forms.ToolStripItemOverflow.Always;
-            this.StatusLabel.Size = new System.Drawing.Size(87, 17);
+            this.StatusLabel.Size = new System.Drawing.Size(280, 17);
             this.StatusLabel.Text = "Not Running ...";
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // LogApp
             // 
@@ -105,7 +118,7 @@
             // 
             this.Gool_services.AutoSize = true;
             this.Gool_services.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.Gool_services.Location = new System.Drawing.Point(228, 162);
+            this.Gool_services.Location = new System.Drawing.Point(166, 127);
             this.Gool_services.Name = "Gool_services";
             this.Gool_services.Size = new System.Drawing.Size(124, 24);
             this.Gool_services.TabIndex = 7;
@@ -116,11 +129,46 @@
             // MenuPanel
             // 
             this.MenuPanel.BackColor = System.Drawing.Color.DarkGray;
+            this.MenuPanel.Controls.Add(this.Restart);
+            this.MenuPanel.Controls.Add(this.SelectLanguage);
             this.MenuPanel.Location = new System.Drawing.Point(12, 38);
             this.MenuPanel.Name = "MenuPanel";
             this.MenuPanel.Size = new System.Drawing.Size(148, 342);
             this.MenuPanel.TabIndex = 8;
             this.MenuPanel.Visible = false;
+            // 
+            // SelectLanguage
+            // 
+            this.SelectLanguage.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem});
+            this.SelectLanguage.Location = new System.Drawing.Point(0, 0);
+            this.SelectLanguage.Name = "SelectLanguage";
+            this.SelectLanguage.Size = new System.Drawing.Size(148, 24);
+            this.SelectLanguage.TabIndex = 0;
+            this.SelectLanguage.Text = "menuStrip1";
+            // 
+            // languageToolStripMenuItem
+            // 
+            this.languageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.englishToolStripMenuItem,
+            this.persianToolStripMenuItem});
+            this.languageToolStripMenuItem.Name = "languageToolStripMenuItem";
+            this.languageToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.languageToolStripMenuItem.Text = "Language";
+            // 
+            // englishToolStripMenuItem
+            // 
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.englishToolStripMenuItem.Text = "English";
+            this.englishToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
+            // 
+            // persianToolStripMenuItem
+            // 
+            this.persianToolStripMenuItem.Name = "persianToolStripMenuItem";
+            this.persianToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.persianToolStripMenuItem.Text = "Persian (فارسی)";
+            this.persianToolStripMenuItem.Click += new System.EventHandler(this.persianToolStripMenuItem_Click);
             // 
             // topPanel
             // 
@@ -134,15 +182,31 @@
             this.topPanel.Size = new System.Drawing.Size(434, 33);
             this.topPanel.TabIndex = 9;
             // 
+            // CloseApp
+            // 
+            this.CloseApp.BackColor = System.Drawing.Color.Gray;
+            this.CloseApp.FlatAppearance.BorderSize = 0;
+            this.CloseApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseApp.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18.25F, System.Drawing.FontStyle.Bold);
+            this.CloseApp.ForeColor = System.Drawing.Color.Red;
+            this.CloseApp.Location = new System.Drawing.Point(401, -2);
+            this.CloseApp.Name = "CloseApp";
+            this.CloseApp.Size = new System.Drawing.Size(33, 34);
+            this.CloseApp.TabIndex = 2;
+            this.CloseApp.Text = "X";
+            this.CloseApp.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.CloseApp.UseVisualStyleBackColor = false;
+            this.CloseApp.Click += new System.EventHandler(this.CloseApp_Click);
+            // 
             // TextLabelTopPanel
             // 
-            this.TextLabelTopPanel.AutoSize = true;
             this.TextLabelTopPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.TextLabelTopPanel.Location = new System.Drawing.Point(145, 5);
+            this.TextLabelTopPanel.Location = new System.Drawing.Point(152, 5);
             this.TextLabelTopPanel.Name = "TextLabelTopPanel";
             this.TextLabelTopPanel.Size = new System.Drawing.Size(126, 20);
             this.TextLabelTopPanel.TabIndex = 1;
             this.TextLabelTopPanel.Text = "Freedom Guard";
+            this.TextLabelTopPanel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // MenuShowHide
             // 
@@ -157,27 +221,103 @@
             this.MenuShowHide.UseVisualStyleBackColor = true;
             this.MenuShowHide.Click += new System.EventHandler(this.MenuShowHide_Click);
             // 
-            // CloseApp
+            // StatusText
             // 
-            this.CloseApp.BackColor = System.Drawing.Color.Gray;
-            this.CloseApp.FlatAppearance.BorderSize = 0;
-            this.CloseApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CloseApp.Font = new System.Drawing.Font("Microsoft Yi Baiti", 18.25F, System.Drawing.FontStyle.Bold);
-            this.CloseApp.ForeColor = System.Drawing.Color.Red;
-            this.CloseApp.Location = new System.Drawing.Point(400, 0);
-            this.CloseApp.Name = "CloseApp";
-            this.CloseApp.Size = new System.Drawing.Size(33, 32);
-            this.CloseApp.TabIndex = 2;
-            this.CloseApp.Text = "X";
-            this.CloseApp.UseVisualStyleBackColor = false;
-            this.CloseApp.Click += new System.EventHandler(this.CloseApp_Click);
+            this.StatusText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StatusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.StatusText.Location = new System.Drawing.Point(191, 60);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(200, 20);
+            this.StatusText.TabIndex = 10;
+            this.StatusText.Text = "Disconnected";
+            this.StatusText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.StatusText.Click += new System.EventHandler(this.StatusText_Click);
+            // 
+            // scan_Warp
+            // 
+            this.scan_Warp.AutoSize = true;
+            this.scan_Warp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.scan_Warp.Location = new System.Drawing.Point(167, 157);
+            this.scan_Warp.Name = "scan_Warp";
+            this.scan_Warp.Size = new System.Drawing.Size(111, 24);
+            this.scan_Warp.TabIndex = 11;
+            this.scan_Warp.Text = "Scan Warp";
+            this.scan_Warp.UseVisualStyleBackColor = true;
+            this.scan_Warp.CheckedChanged += new System.EventHandler(this.scan_Warp_CheckedChanged);
+            // 
+            // PsCountry
+            // 
+            this.PsCountry.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.PsCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.PsCountry.FormattingEnabled = true;
+            this.PsCountry.Items.AddRange(new object[] {
+            "disable",
+            "Austria (AT)",
+            "Belgium (BE)",
+            "Bulgaria (BG)",
+            "Brazil (BR)",
+            "Canada (CA)",
+            "Switzerland (CH)",
+            "Czech Republic (CZ)",
+            "Germany (DE)",
+            "Denmark (DK)",
+            "Estonia (EE)",
+            "Spain (ES)",
+            "Finland (FI)",
+            "France (FR)",
+            "United Kingdom (GB)",
+            "Hungary (HU)",
+            "Ireland (IE)",
+            "India (IN)",
+            "Italy (IT)",
+            "Japan (JP)",
+            "Latvia (LV)",
+            "Netherlands (NL)",
+            "Norway (NO)",
+            "Poland (PL)",
+            "Romania (RO)",
+            "Serbia (RS)",
+            "Sweden (SE)",
+            "Singapore (SG)",
+            "Slovakia (SK)",
+            "Ukraine (UA)",
+            "United States (US)"});
+            this.PsCountry.Location = new System.Drawing.Point(303, 157);
+            this.PsCountry.Name = "PsCountry";
+            this.PsCountry.Size = new System.Drawing.Size(119, 28);
+            this.PsCountry.TabIndex = 12;
+            this.PsCountry.SelectedIndexChanged += new System.EventHandler(this.PsCountry_SelectedIndexChanged);
+            // 
+            // TextShowps
+            // 
+            this.TextShowps.AutoSize = true;
+            this.TextShowps.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.TextShowps.Location = new System.Drawing.Point(304, 130);
+            this.TextShowps.Name = "TextShowps";
+            this.TextShowps.Size = new System.Drawing.Size(118, 18);
+            this.TextShowps.TabIndex = 13;
+            this.TextShowps.Text = "Psiphon Country";
+            // 
+            // Restart
+            // 
+            this.Restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Restart.Location = new System.Drawing.Point(3, 309);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(142, 30);
+            this.Restart.TabIndex = 1;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
             // 
             // FreedomGuard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(434, 405);
-            this.ControlBox = false;
+            this.Controls.Add(this.TextShowps);
+            this.Controls.Add(this.PsCountry);
+            this.Controls.Add(this.scan_Warp);
+            this.Controls.Add(this.StatusText);
             this.Controls.Add(this.topPanel);
             this.Controls.Add(this.MenuPanel);
             this.Controls.Add(this.Gool_services);
@@ -187,14 +327,18 @@
             this.Controls.Add(this.start_Guard);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.MainMenuStrip = this.SelectLanguage;
             this.Name = "FreedomGuard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Freedom Guard";
             this.Load += new System.EventHandler(this.FreedomGuard_Load);
             this.Status.ResumeLayout(false);
             this.Status.PerformLayout();
+            this.MenuPanel.ResumeLayout(false);
+            this.MenuPanel.PerformLayout();
+            this.SelectLanguage.ResumeLayout(false);
+            this.SelectLanguage.PerformLayout();
             this.topPanel.ResumeLayout(false);
-            this.topPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -214,6 +358,15 @@
         private System.Windows.Forms.Button MenuShowHide;
         private System.Windows.Forms.Label TextLabelTopPanel;
         private System.Windows.Forms.Button CloseApp;
+        private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem persianToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip SelectLanguage;
+        private System.Windows.Forms.Label StatusText;
+        private System.Windows.Forms.CheckBox scan_Warp;
+        private System.Windows.Forms.ComboBox PsCountry;
+        private System.Windows.Forms.Label TextShowps;
+        private System.Windows.Forms.Button Restart;
     }
 }
 
--- Freedom-Guard/FreedomGuard.cs
+++ Freedom-Guard/FreedomGuard.cs
@@ -17,6 +17,27 @@ namespace Freedom_Guard
 {
     public partial class FreedomGuard : Form
     {
+        public bool StatusGuard = false;
+        private void add_services(string args)
+        {
+            Switchs = "";
+            services = "";
+            if (Gool_services.Checked)
+            {
+                Switchs += " --gool ";
+                services += " + Gool ";
+            }
+            if (scan_Warp.Checked)
+            {
+                Switchs += " --scan ";
+                services += " + Scan  ";
+            }
+            if (PsCountry.SelectedItem.ToString() != "disable" && PsCountry.SelectedItem.ToString() != "")
+            {
+                Switchs += " --cfon --country " + (PsCountry.SelectedItem.ToString().Substring(PsCountry.SelectedItem.ToString().Length - 4).Replace("(", "").Replace(")", "")); ;
+                services += $" + Psiphon {PsCountry.SelectedItem.ToString()}  ";
+            }
+        }
         public FreedomGuard()
         {
             InitializeComponent();
@@ -29,29 +50,59 @@ namespace Freedom_Guard
         public string Switchs = "";
         private void start_Guard_Click(object sender, EventArgs e)
         {
-            // Start Application
+            // Start Services
             string exePath = "warp-plus.exe";
             Stop_Guard();
             if (System.IO.File.Exists(exePath))
             {
-                ProgressBarStatus.Value = 10;
-                ProcessStartInfo startInfo = new ProcessStartInfo
+                if (StatusGuard == false)
                 {
-                    FileName = exePath,
-                    UseShellExecute = true,
-                    Arguments = Switchs
-                };
-                ProgressBarStatus.Value = 30;
-                process = Process.Start(startInfo);
-                Log("Start Services ...");
-                StatusLabel.Text = "Running Warp + " + services + " ...";
-                string proxyAddress = "127.0.0.1";
-                string proxyPort = "8086";
-                bool proxyEnabled = true;
-                ProgressBarStatus.Value = 100;
-                SetProxySettings(proxyAddress, proxyPort, proxyEnabled);
-                ProgressBarStatus.Value = 0;
-                Log("Set Proxy Server ...");
+                    ProgressBarStatus.Value = 10;
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = exePath,
+                        UseShellExecute = true,
+                        Arguments = Switchs
+                    };
+                    ProgressBarStatus.Value = 30;
+                    process = Process.Start(startInfo);
+                    Log("Start Services ...");
+                    StatusLabel.Text = "Running Warp " + services + " ...";
+                    string proxyAddress = "127.0.0.1";
+                    string proxyPort = "8086";
+                    bool proxyEnabled = true;
+                    ProgressBarStatus.Value = 100;
+                    SetProxySettings(proxyAddress, proxyPort, proxyEnabled);
+                    ProgressBarStatus.Value = 0;
+                    Log("Set Proxy Server ...");
+                    if (Lang == "fa")
+                    {
+                        start_Guard.Text = "قطع اتصال";
+                        StatusText.Text = "متصل شده";
+                    }
+                    else
+                    {
+                        start_Guard.Text = "Disconnect";
+                        StatusText.Text = "Connected";
+                    }
+                    StatusGuard = true;
+                }
+                else
+                {
+                    if (Lang == "fa")
+                    {
+                        start_Guard.Text = "اتصال";
+                        StatusText.Text = "وصل نشده است";
+                    }
+                    else
+                    {
+                        start_Guard.Text = "Connect";
+                        StatusText.Text = "Disconnected";
+                    }
+                    StatusGuard = false;
+                    Stop_Guard();
+                }
+
             }
             else
             {
@@ -75,23 +126,24 @@ namespace Freedom_Guard
             try
             {
                 process.Kill();
-                ProgressBarStatus.Value = 100;
                 string proxyAddress = "127.0.0.1";
                 string proxyPort = "8086";
                 bool proxyEnabled = false;
                 Log("Stoped All Services ...");
                 SetProxySettings(proxyAddress, proxyPort, proxyEnabled);
-                ProgressBarStatus.Value = 0;
+                ProgressBarStatus.Value = 100;
                 Log("Not Set Proxy Server ...");
             }
             catch
             {
 
             }
+            StatusLabel.Text = "Not Running ";
+            ProgressBarStatus.Value = 0;
 
         }
         public string Lang = "en";
-        public string services = "en";
+        public string services = "";
 
         private void FreedomGuard_Load(object sender, EventArgs e)
         {
@@ -102,6 +154,14 @@ namespace Freedom_Guard
                 if (INIApp.Read("gool", "setting") == "true")
                 {
                     Gool_services.Checked = true;
+                }
+                if (INIApp.Read("ps", "setting") != "")
+                {
+                    PsCountry.SelectedIndex = Convert.ToInt32(INIApp.Read("ps", "setting"));
+                }
+                if (INIApp.Read("scan", "setting") == "true")
+                {
+                    scan_Warp.Checked = true;
                 }
                 if (INIApp.Read("runWtStart", "setting") == "true")
                 {
@@ -119,6 +179,7 @@ namespace Freedom_Guard
                     {
                     }
                 }
+                add_services("");
             }
             catch
             {
@@ -127,9 +188,15 @@ namespace Freedom_Guard
             }
             if (Lang == "fa")
             {
-                start_Guard.Text = "شروع";
+                start_Guard.Text = "اتصال";
                 LabelLogApp.Text = "رویداد های برنامه";
                 Gool_services.Text = "سرویس گول";
+                StatusText.Text = "وصل نشده است";
+                TextLabelTopPanel.Text = "گارد آزادی";
+                TextShowps.Text = "کشور سایفون";
+                Restart.Text = "اجرای مجدد برنامه";
+                scan_Warp.Text = "اسکن وارپ";
+                languageToolStripMenuItem.Text = "زبان ها";
             }
         }
         class IniFile   // revision 11
@@ -184,22 +251,18 @@ namespace Freedom_Guard
 
         private void Gool_services_CheckedChanged(object sender, EventArgs e)
         {
+            add_services("");
             if (Gool_services.Checked == false)
             {
-                services = "";
-                Switchs = "";
                 var INIApp = new IniFile("Settings.ini");
                 INIApp.Write("gool", "false", "setting");
             }
             else
             {
-                services = "Gool";
-                Switchs = "--gool";
                 var INIApp = new IniFile("Settings.ini");
                 INIApp.Write("gool", "true", "setting");
             }
         }
-
         private void runServicesWithStartToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var INIApp = new IniFile("Settings.ini");
@@ -252,6 +315,73 @@ namespace Freedom_Guard
         {
             Stop_Guard();
             Application.Exit();
+        }
+
+        private void englishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var INIApp = new IniFile("Settings.ini");
+            INIApp.Write("lang", "en", "setting");
+            Application.Restart();
+        }
+
+        private void persianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var INIApp = new IniFile("Settings.ini");
+            INIApp.Write("lang", "fa", "setting");
+            Application.Restart();
+        }
+
+        public void Console(string SwitchsOne)
+        {
+            string exePath = "warp-plus.exe";
+            Stop_Guard();
+            if (System.IO.File.Exists(exePath))
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = exePath,
+                    UseShellExecute = true,
+                    Arguments = SwitchsOne
+                };
+                process = Process.Start(startInfo);
+            }
+            process.WaitForExit();
+        }
+        private void scan_Warp_CheckedChanged(object sender, EventArgs e)
+        {
+            add_services("");
+            if (scan_Warp.Checked == false)
+            {
+                var INIApp = new IniFile("Settings.ini");
+                INIApp.Write("scan", "false", "setting");
+            }
+            else
+            {
+                var INIApp = new IniFile("Settings.ini");
+                INIApp.Write("scan", "true", "setting");
+            }
+        }
+
+        private void PsCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (PsCountry.SelectedItem.ToString() != "disable" & PsCountry.SelectedItem.ToString() != "")
+            {
+                Gool_services.Checked = false;
+            }
+            add_services("");
+            var INIApp = new IniFile("Settings.ini");
+            INIApp.Write("ps", PsCountry.SelectedIndex.ToString(), "setting");
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            Stop_Guard();
+            Application.Restart();
+        }
+
+        private void StatusText_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
