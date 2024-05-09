f14faee2bdb086e0464b36bc608baad397b7c33f
fwldom <fwldom@duck.com>
2024-05-09 18:30:42 +0330

    Update 1.2.2 Full

--- Freedom-Guard/Properties/AssemblyInfo.cs
+++ Freedom-Guard/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e69dc5d2-be96-4dfc-8161-4948b0100a4e")]
